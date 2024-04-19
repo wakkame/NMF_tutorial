722787b78b8cba7a7d9dac06123730b5a7225ea7
s-taninomiya <st20113@t.kagawa-nct.ac.jp>
2024-04-19 13:10:13 +0900

    #1 Step1 行列名の変更

--- NMF.m
+++ NMF.m
@@ -5,22 +5,22 @@ I = 4;
 J = 5;
 R = 3;
 
-% generate basisMatrix, coefficientMatrix
-basisMatrix = generateFullrankMatrix(I, R);
-coefficientMatrix = generateFullrankMatrix(R, J);
+% generate X1, X2
+X1 = generateFullrankMatrix(I, R);
+X2 = generateFullrankMatrix(R, J);
 
 % caluclate X
-X = basisMatrix * coefficientMatrix;
+X = X1 * X2;
 
 % check matrix
 disp(X);
-disp(basisMatrix);
-disp(coefficientMatrix);
+disp(X1);
+disp(X2);
 
 % check ranks
 disp(rank(X));
-disp(rank(basisMatrix));
-disp(rank(coefficientMatrix));
+disp(rank(X1));
+disp(rank(X2));
 
 function fullrankMatrix = generateFullrankMatrix(row, column)
 fullrankMatrix = zeros(row, column);
