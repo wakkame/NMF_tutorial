56bdda2840ddd50d01473e5d209c1eedc9b219c6
s-taninomiya <st20113@t.kagawa-nct.ac.jp>
2024-04-19 16:02:08 +0900

    #5 Step4 編集中

--- main.m
+++ main.m
@@ -1,36 +1,39 @@
 clear; close all; clc;
 
 % define some variable
-I = 100;
-J = 200;
-R = 10;
-K = 10;
+windowLength = 2 ^ 11;
+shiftLength = 2 ^ 10;
+windowType = "han";
+paddingMethod = "end";
+K = 20;
 nIter = 1000;
 
-% generate X1, X2
-X1 = generateFullrankMatrix(I, R);
-X2 = generateFullrankMatrix(R, J);
+% loading audio files
+[inputSignal, fs] = audioread("guitar.wav");
 
-% calculate X
-X = X1 * X2;
+% calculate STFT
+F = DGTtool("windowShift", shiftLength, ...
+            "windowLength", windowLength, ...
+            "FFTnum", windowLength, ...
+            "windowName", windowType);
+S = F(inputSignal);
+F.plot(inputSignal, fs);
+
+% calulate amplitude spectrogram
+X = abs(S);
 
 % execute calcNMF
 [W, H, WH] = calcNMF(X, ...
                      "K", K, ...
                      "nIter", nIter);
 
+% check 
+
 % check matrixs
 displayColorMap(X);
 displayColorMap(W);
 displayColorMap(H);
 displayColorMap(WH);
-
-function fullrankMatrix = generateFullrankMatrix(row, column)
-fullrankMatrix = zeros(row, column);
-while rank(fullrankMatrix) < min(row, column)
-    fullrankMatrix = rand(row, column);
-end
-end
 
 function [] = displayColorMap(matrix)
 figure;
